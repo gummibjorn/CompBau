928c8e5e6c6b7e6ed3a3a91b7d56d04c48f9bdf8
tourn <latzer.daniel@gmail.com>
2017-04-20 14:30:18 +0200

    Reformat interpreter

--- VirtualMachine/Runtime/Interpreter.cs
+++ VirtualMachine/Runtime/Interpreter.cs
@@ -2,123 +2,158 @@
 using RappiSharp.VirtualMachine.Descriptors;
 using RappiSharp.VirtualMachine.Error;
 
-namespace RappiSharp.VirtualMachine.Runtime {
-  internal class Interpreter {
-    private readonly Loader _loader;
-    private readonly CallStack _callStack;
+namespace RappiSharp.VirtualMachine.Runtime
+{
+    internal class Interpreter
+    {
+        private readonly Loader _loader;
+        private readonly CallStack _callStack;
         private IConsole _console;
 
-    public Interpreter(Metadata metadata) : this(metadata, new SystemConsole()) { }
+        public Interpreter(Metadata metadata) : this(metadata, new SystemConsole()) { }
 
-    public Interpreter(Metadata metadata, IConsole console) {
+        public Interpreter(Metadata metadata, IConsole console)
+        {
             _console = console;
-      _loader = new Loader(metadata);
-      _callStack = new CallStack();
-    }
-
-    public void Run() {
-      SetProgramEntry();
-      Interpret();
-    }
-
-    private void SetProgramEntry() {
-      var mainMethod = _loader.MainMethod;
-      var mainClass = mainMethod.Parent;
-      if (mainMethod.ReturnType != null || mainMethod.ParameterTypes.Length != 0) {
-        throw new InvalidILException("Invalid main method");
-      }
-      var mainObject = NewObject(mainClass);
-      var locals = InitializedVariables(mainMethod.LocalTypes);
-      _callStack.Push(new ActivationFrame(mainMethod, mainObject, new object[0], locals));
-    }
-
-    private object NewObject(ClassDescriptor mainClass) {
-      // TODO: Implement object orientation later
-      return null;
-    }
-
-    private void Interpret() {
-      // TODO: Implement interpreter
-    }
-
-    private object[] InitializedVariables(TypeDescriptor[] types) {
-      var variables = new object[types.Length];
-      for (int index = 0; index < variables.Length; index++) {
-        variables[index] = DefaultValue(types[index]);
-      }
-      return variables;
-    }
-
-    private object DefaultValue(TypeDescriptor type) {
-      if (type is InbuiltType) {
-        if (type == InbuiltType.Bool) {
-          return false;
+            _loader = new Loader(metadata);
+            _callStack = new CallStack();
         }
-        if (type == InbuiltType.Char) {
-          return '\0';
+
+        public void Run()
+        {
+            SetProgramEntry();
+            Interpret();
         }
-        if (type == InbuiltType.Int) {
-          return 0;
+
+        private void SetProgramEntry()
+        {
+            var mainMethod = _loader.MainMethod;
+            var mainClass = mainMethod.Parent;
+            if (mainMethod.ReturnType != null || mainMethod.ParameterTypes.Length != 0)
+            {
+                throw new InvalidILException("Invalid main method");
+            }
+            var mainObject = NewObject(mainClass);
+            var locals = InitializedVariables(mainMethod.LocalTypes);
+            _callStack.Push(new ActivationFrame(mainMethod, mainObject, new object[0], locals));
         }
-        if (type == InbuiltType.String) {
-          return string.Empty;
+
+        private object NewObject(ClassDescriptor mainClass)
+        {
+            // TODO: Implement object orientation later
+            return null;
         }
-        throw new InvalidILException("Invalid inbuilt type");
-      } else {
-        return null;
-      }
-    }
 
-    private ActivationFrame ActiveFrame {
-      get {
-        return _callStack.Peek();
-      }
-    }
+        private void Interpret()
+        {
+            // TODO: Implement interpreter
+        }
 
-    private MethodDescriptor ActiveMethod {
-      get {
-        return ActiveFrame.Method;
-      }
-    }
+        private object[] InitializedVariables(TypeDescriptor[] types)
+        {
+            var variables = new object[types.Length];
+            for (int index = 0; index < variables.Length; index++)
+            {
+                variables[index] = DefaultValue(types[index]);
+            }
+            return variables;
+        }
 
-    private object[] Locals {
-      get {
-        return ActiveFrame.Locals;
-      }
-    }
+        private object DefaultValue(TypeDescriptor type)
+        {
+            if (type is InbuiltType)
+            {
+                if (type == InbuiltType.Bool)
+                {
+                    return false;
+                }
+                if (type == InbuiltType.Char)
+                {
+                    return '\0';
+                }
+                if (type == InbuiltType.Int)
+                {
+                    return 0;
+                }
+                if (type == InbuiltType.String)
+                {
+                    return string.Empty;
+                }
+                throw new InvalidILException("Invalid inbuilt type");
+            }
+            else
+            {
+                return null;
+            }
+        }
 
-    private object[] Arguments {
-      get {
-        return ActiveFrame.Arguments;
-      }
-    }
+        private ActivationFrame ActiveFrame
+        {
+            get
+            {
+                return _callStack.Peek();
+            }
+        }
 
-    private EvaluationStack Stack {
-      get {
-        return ActiveFrame.EvaluationStack;
-      }
-    }
+        private MethodDescriptor ActiveMethod
+        {
+            get
+            {
+                return ActiveFrame.Method;
+            }
+        }
 
-    private object ThisReference {
-      get {
-        return ActiveFrame.ThisReference;
-      }
-    }
+        private object[] Locals
+        {
+            get
+            {
+                return ActiveFrame.Locals;
+            }
+        }
 
-    private int InstructionPointer {
-      get {
-        return ActiveFrame.InstructionPointer;
-      }
-      set {
-        ActiveFrame.InstructionPointer = value;
-      }
-    }
+        private object[] Arguments
+        {
+            get
+            {
+                return ActiveFrame.Arguments;
+            }
+        }
 
-    private T Verify<T>(object value) {
-      if (!(value is T)) {
-        throw new InvalidILException($"Expected {typeof(T)} instead of {value}");
-      }
-      return (T)value;
+        private EvaluationStack Stack
+        {
+            get
+            {
+                return ActiveFrame.EvaluationStack;
+            }
+        }
+
+        private object ThisReference
+        {
+            get
+            {
+                return ActiveFrame.ThisReference;
+            }
+        }
+
+        private int InstructionPointer
+        {
+            get
+            {
+                return ActiveFrame.InstructionPointer;
+            }
+            set
+            {
+                ActiveFrame.InstructionPointer = value;
+            }
+        }
+
+        private T Verify<T>(object value)
+        {
+            if (!(value is T))
+            {
+                throw new InvalidILException($"Expected {typeof(T)} instead of {value}");
+            }
+            return (T)value;
+        }
     }
-  }
 }
